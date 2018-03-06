1f712f5dbadc9efbcced41dab3f155a5e63285b0
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-03-07 01:27:48 +0900

    saved decorrelation.jl

--- JuliaScripts/Decorrelation.jl
+++ JuliaScripts/Decorrelation.jl
@@ -2,6 +2,15 @@ using Plots, RDatasets, Query, DataFrames, StatPlots
 
 iris = dataset("datasets", "iris")
 
+function SplitDFbySpecies(iris, species)
+    df = @from i in iris begin
+         @where i.Species == species
+         @select {i.PetalLength, i.PetalWidth}
+         @collect DataFrame
+    end
+    return df
+end
+
 function dfmean(df::DataFrame)
     return [mean(i) for i in df.columns]
 end
@@ -20,13 +29,39 @@ function dfcov(df)
     return array
 end
 
-function lintf(x, S)
-    return S' * x
+function lintf(df, S)
+    groups = Array(df[:Species])
+    petals = df[[:PetalLength, :PetalWidth]]
+
+    colnames = names(petals)
+    values = Array(petals)
+    for i in 1:size(values)[1]
+        values[i, :] = S' * values[i, :]
+    end
+
+    tf_df = DataFrame()
+    tf_df[colnames[1]] = values[:, 1]
+    tf_df[colnames[2]] = values[:, 2]
+    tf_df[:Species] = groups
+    return tf_df
 end
 
-coviris = dfcov(iris[[:PetalLength, :PetalWidth]])
-eigveciris = eigvecs(coviris)
-y = lintf(iris[[:PetalLength, :PetalWidth]], S)
+@df iris scatter(:PetalLength, :PetalWidth, group=:Species,
+                 m=(0.5, [:+ :h :star7], 12), bg=RGB(.2,.2,.2))
 
-df = DataFrame(y)
-@df iris scatter(:PetalLength, :PetalWidth, group=:Speicies)
+scatter!(xlabel="Petal Length", ylabel="Petal Width", title="No Decorrelation",
+         xlims=(1, 7), ylims=(-1, 4))
+savefig("../figures/no_decorrelation.png")
+
+coviris = dfcov(iris[[:PetalLength, :PetalWidth]])
+S = eigvecs(coviris)
+
+decorrelated = lintf(iris, S)
+covdecorrelated = dfcov(decorrelated[[:PetalLength, :PetalWidth]])
+println(covdecorrelated)
+
+@df decorrelated scatter(:PetalLength, :PetalWidth, group=:Species,
+                         m=(0.5, [:+ :h :star7], 12), bg=RGB(.2,.2,.2))
+scatter!(xlabel="Petal Length", ylabel="Petal Width", title="Decorrelated",
+         xlims=(-2, 3), ylims=(-7, -1))
+savefig("../figures/decorrelation.png")
