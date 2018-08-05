f843306526ed4e8a112fcb86b92529b82fce3899
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-08-05 23:09:21 +0900

    次はbnpyを試してみる

--- sound_recognition/scripts/mfcc_trial.py
+++ sound_recognition/scripts/mfcc_trial.py
@@ -1,22 +1,24 @@
 import os
 import librosa
+import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def load_data(path):
+def load_data(path, sr=16000):
     base_path = "/home/hidehisa/hobby/pattern_recognition/sound_recognition" + \
                 "/scripts"
     if os.getcwd() != base_path:
         os.chdir(base_path)
 
     x, fs = librosa.load(path, sr=44100)
-    mfccs = librosa.feature.mfcc(x, sr=fs)
-    return mfccs
+    mfccs, sr = librosa.feature.mfcc(x, sr=sr)
+    return mfccs, sr
 
 
 if __name__ == '__main__':
     path = "../raw_data/lab_meeting.wav"
-    mfccs = load_data(path)
-    
+    mfccs,sr = load_data(path)
+    librosa.display.specshow(mfccs[:, :10], sr=sr, x_axis='time')
+    plt.show()
